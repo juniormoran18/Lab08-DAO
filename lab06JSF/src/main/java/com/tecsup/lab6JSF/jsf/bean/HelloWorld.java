5dab915e1a117d96c50c9f637772ab0275ec3686
Junior <junior.moran@tecsup.edu.pe>
2017-09-20 13:33:52 -0500

    commit1

---
+++ lab06JSF/src/main/java/com/tecsup/lab6JSF/jsf/bean/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.tecsup.lab6JSF.jsf.bean;
+import javax.faces.bean.ManagedBean;
+
+
+@ManagedBean(name = "helloWorld", eager = true)
+
+public class HelloWorld {
+
+	private String message = "Hello World...!";
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message + "----";
+	}
+
+}
